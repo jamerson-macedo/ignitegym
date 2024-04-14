991e043b080503ae2095d6bfc8926c171b217408
jamerson Macedo <jamerson_macedo@hotmail.com>
2024-04-13 21:55:27 -0300

    configurando thema

--- App.tsx
+++ App.tsx
@@ -2,12 +2,14 @@
 import { StatusBar, StyleSheet, Text, View } from 'react-native';
 import { NativeBaseProvider,Box } from 'native-base';
 import { useFonts,Roboto_400Regular,Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { Loading } from '@components/Loading';
+import {THEME} from './src/theme'
 export default function App() {
   const [fontLoaded]=useFonts({Roboto_400Regular,Roboto_700Bold});
   return (
-    <NativeBaseProvider>
+    <NativeBaseProvider theme={THEME}>
     <StatusBar barStyle={'light-content'} backgroundColor={"transparent"} translucent/>
-      {fontLoaded?<View/>:<View/>}
+     {fontLoaded? <View/>: <Loading/>}
     </NativeBaseProvider>
   );
 }
