4c94622d6c6e75388b2fbf34ddcbbca635826674
jamerson Macedo <jamerson_macedo@hotmail.com>
2024-04-13 21:40:28 -0300

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
+import { StatusBar, StyleSheet, Text, View } from 'react-native';
+import { NativeBaseProvider,Box } from 'native-base';
+import { useFonts,Roboto_400Regular,Roboto_700Bold } from '@expo-google-fonts/roboto';
 export default function App() {
+  const [fontLoaded]=useFonts({Roboto_400Regular,Roboto_700Bold});
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider>
+    <StatusBar barStyle={'light-content'} backgroundColor={"transparent"} translucent/>
+      {fontLoaded?<View/>:<View/>}
+    </NativeBaseProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
