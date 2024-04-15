8f2726ee1890c648ef60ed93b80d9ddc18263c10
jamerson Macedo <jamerson_macedo@hotmail.com>
2024-04-15 18:30:03 -0300

    card de historico

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,3 +1,4 @@
+import { HistoryCard } from "@components/HistoryCard";
 import { ScreenHeader } from "@components/ScreenHeader";
 import { Center,Text, VStack } from "native-base";
 
@@ -6,6 +7,7 @@ export function History(){
     return(
        <VStack flex={1}>
         <ScreenHeader title='Historico de exercicio'/>
+        <HistoryCard/>
        </VStack>
     );
 }
