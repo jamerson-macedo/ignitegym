4a8d09d7d518ad27bd542902015964ff6672194a
jamerson Macedo <jamerson_macedo@hotmail.com>
2024-04-15 18:48:11 -0300

    tela de historico concluido

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,13 +1,36 @@
 import { HistoryCard } from "@components/HistoryCard";
 import { ScreenHeader } from "@components/ScreenHeader";
-import { Center,Text, VStack } from "native-base";
+import { Heading, VStack,SectionList, Text, Center } from "native-base";
+import { useState } from "react";
 
-export function History(){
+export function History() {
+    // estruturea da sextion list
+  const [exercises, setExercises] = useState([
+    {
+      title: "24.2.2024",
+      data: ["puxada lateral", "puxada frontal"],
+    },
+  ]);
 
-    return(
-       <VStack flex={1}>
-        <ScreenHeader title='Historico de exercicio'/>
-        <HistoryCard/>
-       </VStack>
-    );
+  return (
+    <VStack flex={1}>
+      <ScreenHeader title="Historico de exercicio" />
+      <SectionList sections={exercises} 
+      contentContainerStyle={exercises.length===0&&{flex:1, justifyContent:"center"}}
+      ListEmptyComponent={()=>(
+        <Text color={"gray.100"} textAlign={"center"}>Nào há exercicios registrados ainda.{'\n'}
+            vamos fazer exercicios hoje?
+        </Text>
+      )}
+      keyExtractor={item=>item}
+      renderItem={({item})=>(<HistoryCard />)}
+      renderSectionHeader={({section})=>(
+        <Heading color={"gray.200"} fontSize={"md"} mt={10} mb={3}>{section.title}</Heading>
+
+      )}
+      px={8}
+      />
+      
+    </VStack>
+  );
 }
