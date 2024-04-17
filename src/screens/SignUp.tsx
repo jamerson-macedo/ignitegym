b1f92f5e38e6ded832e43b973b038237f59477fc
jamerson Macedo <jamerson_macedo@hotmail.com>
2024-04-17 20:28:54 -0300

    implementando hookform

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -4,63 +4,110 @@ import LogoSVG from "@assets/logo.svg";
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
 import { useNavigation } from "@react-navigation/native";
-
+import { useForm, Controller } from "react-hook-form";
 
 // VS STACK COLOCA UMA COISA EM CIMA DA OUTRA
 // contain ajusta melhor
 // absolute deixa completo pegando tudo
 // margin vertical de 24 em cima e em baixo
 export function SignUp() {
+  const { control } = useForm();
+  
 
-  const navigation=useNavigation();
+  const navigation = useNavigation();
 
-  function handleGoBack(){
+  function handleGoBack() {
     navigation.goBack();
   }
+  function handleSingUp() {
+    console.log(name);
+  }
   return (
-
-    <ScrollView contentContainerStyle={{flexGrow:1}} showsVerticalScrollIndicator={false}>
-
-    
-    <VStack flex={1}  px={8}>
-      <Image
-        source={BackGroundImg}
-        defaultSource={BackGroundImg} 
-        alt="Pessoas treinando"
-        resizeMode="contain"
-        position={"absolute"}
-      />
-      <Center my={24}>
-        <LogoSVG />
-        <Text color={"gray.100"} fontSize={"sm"}>
-          Treine sua mente e o seu corpo
-        </Text>
-      </Center>
-      <Center>
-        <Heading
-          color={"gray.100"}
-          fontSize={"xl"}
-          mb={6}
-          fontFamily={"heading"}
-        >
-          Crie Sua conta
-        </Heading>
-        <Input
-          placeholder="Nome"
-      
+    <ScrollView
+      contentContainerStyle={{ flexGrow: 1 }}
+      showsVerticalScrollIndicator={false}
+    >
+      <VStack flex={1} px={8}>
+        <Image
+          source={BackGroundImg}
+          defaultSource={BackGroundImg}
+          alt="Pessoas treinando"
+          resizeMode="contain"
+          position={"absolute"}
         />
-          <Input
-          placeholder="E-mail"
-          keyboardType="email-address"
-          autoCapitalize="none"
-        />
-        <Input placeholder="Senha" secureTextEntry />
-        <Button title="Criar e Acessar" />
-      </Center >
+        <Center my={24}>
+          <LogoSVG />
+          <Text color={"gray.100"} fontSize={"sm"}>
+            Treine sua mente e o seu corpo
+          </Text>
+        </Center>
+        <Center>
+          <Heading
+            color={"gray.100"}
+            fontSize={"xl"}
+            mb={6}
+            fontFamily={"heading"}
+          >
+            Crie Sua conta
+          </Heading>
 
-        <Button  mt={24}title="Voltar para Login" variant={"outline"} onPress={handleGoBack} />
-    
-    </VStack>
+          <Controller
+            name="name"
+            render={({ field: { onChange, value } }) => (
+              <Input placeholder="Nome" onChangeText={onChange} value={value} />
+            )}
+            control={control}
+          />
+
+          <Controller
+            name="email"
+            control={control}
+            render={({ field: { onChange, value } }) => (
+              <Input
+                placeholder="E-mail"
+                keyboardType="email-address"
+                autoCapitalize="none"
+                onChangeText={onChange}
+                value={value}
+              />
+            )}
+          />
+          <Controller
+            name="password"
+            control={control}
+            render={({ field: { value, onChange } }) => (
+              <Input
+                placeholder="Senha"
+                secureTextEntry
+                onChangeText={onChange}
+                value={value}
+              />
+            )}
+          />
+
+          <Controller
+            name="password_confirm"
+            control={control}
+            render={({ field: { value, onChange } }) => (
+              <Input
+                placeholder="Confirme a Senha"
+                secureTextEntry
+                onChangeText={onChange}
+                value={value}
+              />
+            )}
+          />
+
+          <Button title="Criar e Acessar" onPress={handleSingUp} />
+        </Center>
+
+        <Button
+          mt={24}
+          title="Voltar para Login"
+          variant={"outline"}
+          onPress={handleGoBack}
+        />
+      </VStack>
     </ScrollView>
   );
 }
